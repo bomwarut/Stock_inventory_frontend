f195d77ed3213495f2537ceac7e2055fe09a21e7
Warut Jantasri <89127532+bomwarut@users.noreply.github.com>
2024-07-03 19:56:55 +0700

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+let user = {
+  name: "wer"
+};
+let content = {};
+let isLoggedIn = false;
+
+const products = [
+  { title: 'Cabbage', id: 1, selected: false },
+  { title: 'Garlic', id: 2, selected: true },
+  { title: 'Apple', id: 3, selected: false },
+];
+
+const listItems = products.map(product =>
+  <li key={product.id} style={{ color: product.selected ? 'green' : 'pink'}}>
+    {product.title}
+  </li>
+);
+
+if (isLoggedIn) {
+  content = <MyButton />;
+} else {
+  content = <Ver />;
+}
+
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={handleClick} className="select" >I'm a button</button>
   );
 }
 
-export default App;
+function Ver() {
+  return (
+    <button style={{ color: "red" }}> {user.name}</button >
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <h2>dis play component in react that is create function and return in HTML tag like this:  <MyButton />
+        <Ver /> actually ther are {'<MyButton/>'} and {'<Ver/>'}</h2>
+      <h2>if content result is "{content}"</h2>
+      <h2>list item by map function and if product.selected make it green</h2>
+      {listItems}
+    </div>
+  );
+}
