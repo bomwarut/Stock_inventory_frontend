d6030f8b44c79d10da28be774664033a74e03e8d
Warut Jantasri <89127532+bomwarut@users.noreply.github.com>
2024-07-05 01:11:37 +0700

    insert update select withdraw

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const MyApp = () => {
-  const [stocks, setStocks] = useState([]);
-  useEffect( () => {
-    const getProduct = async () => {
+  const [products, setProducts] = useState([]);
+  const [stock, setStock] = useState([]);
+  const [history, setHistory] = useState([]);
+  const [productQuantities, setProductQuantities] = useState({});
+  const [productQuantities2, setProductQuantities2] = useState({});
+
+  useEffect(() => {
+    const fetchProducts = async () => {
       try {
-        console.log("getting product...");
-        const response = await fetch(`http://localhost:5077/Product`, {
+        const response = await fetch('http://localhost:5077/Product', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
@@ -16,71 +20,222 @@ const MyApp = () => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
-        // console.log(await response.json());
-        // Optionally fetch updated data after action
-        return await response.json();
+        const result = await response.json();
+        setProducts(result.showdata);
       } catch (error) {
-        console.error('Error updating stock:', error);
+        console.error('Error fetching products:', error);
       }
     };
-
-    (async ()=>{
-      let products = (await getProduct()).allProduct;
-      let bodyTable = document.querySelector("#body-product")
-      products.forEach(element => {
-        console.log("kuyyyyyyyyyyyy"+element);
-          bodyTable.innerHTML += `<tr><td>${element.product_name}</td></tr>`;
-      });
-    })();
-   
+    fetchProducts();
   }, []);
 
-  const fetchStocks = async () => {
-    try {
-      const response = await fetch('http://localhost:5077/weatherforecast');
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+  useEffect(() => {
+    const fetchStock = async () => {
+      try {
+        const response = await fetch('http://localhost:5077/Product/stocks', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setStock(result.showdata);
+      } catch (error) {
+        console.error('Error fetching stock:', error);
       }
-      const data = await response.json();
-      setStocks(data);
-    } catch (error) {
-      console.error('Error fetching stocks:', error);
-    }
-  };
+    };
+    fetchStock();
+  }, []);
 
-  const handleStockAction = async (stockId) => {
+  useEffect(() => {
+    const fetchHistory = async () => {
+      try {
+        const response = await fetch('http://localhost:5077/Product/history', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setHistory(result.showdata);
+      } catch (error) {
+        console.error('Error fetching history:', error);
+      }
+    };
+    fetchHistory();
+  }, []);
+
+  const addStock = async (productId) => {
+    const quantity = productQuantities[productId] || 1;
     try {
-      const response = await fetch(`https://localhost:5001/api/updateStock/${stockId}`, {
+      const response = await fetch('http://localhost:5077/Product/addStock', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ /* Any payload data if needed */ }),
+        body: JSON.stringify({ productId, quantity }),
       });
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
-      // Optionally fetch updated data after action
-      fetchStocks();
+      const result = await response.json();
+      if (result.success) {
+        if (result.updatedStock) {
+          setStock(stock.map(s => s.stock_id === result.updatedStock.stock_id ? result.updatedStock : s));
+        } else if (result.newStock) {
+          setStock([...stock, result.newStock]);
+        }
+      } else {
+        console.error('Failed to add stock');
+      }
     } catch (error) {
-      console.error('Error updating stock:', error);
+      console.error('Error adding stock:', error);
     }
   };
 
+  const withdrawstock = async (productId) => {
+    const quantity = productQuantities2[productId] || 1;
+    try {
+      const response = await fetch('http://localhost:5077/Product/withdrawstock', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ productId, quantity }),
+      });
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const result = await response.json();
+      if (result.success) {
+        if (result.updatedStock) {
+          setStock(stock.map(s => s.stock_id === result.updatedStock.stock_id ? result.updatedStock : s));
+        } else if (result.newStock) {
+          setStock([...stock, result.newStock]);
+        }
+      } else {
+        console.error('Failed to add stock');
+      }
+    } catch (error) {
+      console.error('Error adding stock:', error);
+    }
+  };
+
+  const handleQuantityChange = (productId, quantity) => {
+    setProductQuantities({
+      ...productQuantities,
+      [productId]: quantity,
+    });
+  };
+
+  const handleQuantityChange2 = (productId, quantity) => {
+    setProductQuantities2({
+      ...productQuantities2,
+      [productId]: quantity,
+    });
+  };
 
   return (
-    <div>
+    <div id='background'>
       <h1>Welcome to my Stock App</h1>
+
+      <h2>ตารางสินค้า</h2>
       <table>
         <thead>
-          <tr key="test111">
-            <th>ID</th>
-      
+          <tr>
+            <th className='textleft'>ชื่อสินค้า</th>
+            <th>รหัสสินค้า</th>
+            <th className='textright'>ราคา</th>
+            <th className='textright'>จำนวน</th>
+            <th></th>
           </tr>
         </thead>
-        <tbody id="body-product">
-     
-       
+        <tbody>
+          {products.map(product => (
+            <tr key={product.product_id}>
+              <td className='textleft'>{product.product_name}</td>
+              <td>{product.product_code}</td>
+              <td className='textright'>{product.product_price}</td>
+              <td>
+                <input className='textright'
+                  type="number"
+                  value={productQuantities[product.product_id] || 1}
+                  onChange={(e) => handleQuantityChange(product.product_id, parseInt(e.target.value))}
+                />
+              </td>
+              <td>
+                <button type='button' onClick={() => addStock(product.product_id)}>
+                  เพิ่มสินค้าเข้าคลัง
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h2>ตารางคลังสินค้า</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>stock id</th>
+            <th>product id</th>
+            <th className='textleft'>ชื่อสินค้า</th>
+            <th className='textright'>จำนวนสินค้าในคลัง</th>
+            <th>วันทีเพิ่ม</th>
+            <th className='textright'>จำนวนเบิกออก</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {stock.map(stockItem => (
+            <tr key={stockItem.stock_id}>
+              <td>{stockItem.stock_id}</td>
+              <td>{stockItem.product_id}</td>
+              <td className='textleft'>{stockItem.product_name}</td>
+              <td className='textright'>{stockItem.product_quantity}</td>
+              <td>{stockItem.stock_update_at}</td>
+              <td>
+                <input className='textright'
+                  type="number"
+                  value={productQuantities2[stockItem.product_id] || 1}
+                  onChange={(e) => handleQuantityChange2(stockItem.product_id, parseInt(e.target.value))}
+                />
+              </td>
+              <td>
+                <button className="btn btn-primary" type='button' onClick={() => withdrawstock(stockItem.product_id)}>
+                  เบิกออก
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h2>ตารางรายการ รับ เบิก สินค้าคงคลัง</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>history id</th>
+            <th>stock id</th>
+            <th className='textright'>จำนวนสินค้าในคลัง</th>
+            <th>วันทีเพิ่ม</th>
+          </tr>
+        </thead>
+        <tbody>
+          {history.map(historyItem => (
+            <tr key={historyItem.stock_history_id}>
+              <td>{historyItem.stock_history_id}</td>
+              <td>{historyItem.stock_id}</td>
+              <td className='textright'>{historyItem.quantity}</td>
+              <td>{historyItem.stock_history_update_at}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
